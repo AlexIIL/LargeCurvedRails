12fc196645dc2b5e43492d9bbc1ade35c2fb44d6
AlexIIL <alexj9@me.com>
2016-01-05 21:10:17 +0000

    First commit

---
+++ src/main/java/alexiil/mods/traincraft/DefaultProps.java
@@ -0,0 +1,7 @@
+package alexiil.mods.traincraft;
+
+public class DefaultProps {
+    public static final String MODID = "traincraft";
+    public static final String VERSION = "@VERSION@";
+    public static final String MINECRAFT_VERSION = "@MC_VERSION@";
+}
---
+++ src/main/java/alexiil/mods/traincraft/TrainCraft.java
@@ -0,0 +1,8 @@
+package alexiil.mods.traincraft;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = DefaultProps.MODID, name = "TrainCraft")
+public class TrainCraft {
+
+}
